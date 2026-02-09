0828d3a0a542d70fe0ecbbf5dd472309cf5f454e
andredot <andrea.pedot@ubep.unipd.it>
2026-02-09 23:05:58 +0000

    Quarto Report parametrised to cover all three areas

---
+++ build_reports.R
@@ -0,0 +1,9 @@
+library(quarto)
+areas <- c("Lombardia", "Milano", "Valtellina")
+for (a in areas) {
+  quarto_render(
+    input = "report.qmd",
+    execute_params = list(area = a),
+    output_file = paste0("EWS_Report_", a, ".html")
+  )
+  }
