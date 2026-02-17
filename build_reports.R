801d85c9d7cf6a2743c9f7db9b1b5470c85add64
andredot <andrea.pedot@ubep.unipd.it>
2026-02-17 08:57:35 +0000

    Validation report introduced

--- build_reports.R
+++ build_reports.R
@@ -1,9 +1,14 @@
 library(quarto)
 areas <- c("Lombardia", "Milano", "Valtellina")
 for (a in areas) {
+  # quarto_render(
+  #   input = "report.qmd",
+  #   execute_params = list(area = a),
+  #   output_file = paste0("EWS_Report_", a, ".html")
+  # )
   quarto_render(
-    input = "report.qmd",
+    input = "validation_report.qmd",
     execute_params = list(area = a),
-    output_file = paste0("EWS_Report_", a, ".html")
+    output_file = paste0("EWS_Validation_Report_", a, ".html")
   )
-  }
+}
